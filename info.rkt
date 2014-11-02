446f54cc9e143d4641bf49a5f96823014b0ba152
Stephen De Gabrielle <spdegabrielle@gmail.com>
2014-11-02 22:14:56 +0000

    updating for deployment

--- info.rkt
+++ info.rkt
@@ -1,8 +1,46 @@
 #lang info
-(define name "Insert Large Letters")
+(define collection "insert-large-letters")
+(define name "Insert Large Letters Plugin")
 (define version "0.0.1")
 (define release-notes '("for 5.x"))
 (define blurb '("copied/moved from DrRacket."))
 
-(define drracket-name "Insert Large Letters")
-(define drracket-tools (list (list "tool.rkt")))
+(define drracket-tools '(("tool.rkt")))
+(define drracket-tool-names '("Insert Large Letters Plugin"))
+(define drracket-tool-icons '("images/icon.png"))
+(define drracket-tool-urls (list (list "https://github.com/spdegabrielle/insert-large-letters")))
+
+(define required-core-version  "5.0")
+(define repositories           '("4.x"))
+(define categories             '(devtools))
+(define can-be-loaded-with  'none)
+(define primary-file        "tool.rkt")
+;                                     
+;                                     
+;   ;                                 
+;   ;                                 
+;   ;      ;;;;    ;;;;   ;;;;   ;;;  
+;   ;          ;   ;;  ; ;;  ;  ;;  ; 
+;   ;          ;   ;     ;   ;  ;   ;;
+;   ;       ;;;;   ;     ;   ;  ;;;;;;
+;   ;      ;   ;   ;     ;   ;  ;     
+;   ;      ;   ;   ;     ;; ;;  ;     
+;   ;;;;;;  ;;;;   ;      ;;;;   ;;;; 
+;                            ;        
+;                         ;  ;        
+;                          ;;         
+
+;                                                   
+;                                                   
+;   ;               ;      ;                        
+;   ;               ;      ;                        
+;   ;       ;;;   ;;;;;  ;;;;;   ;;;    ;;;;   ;;;  
+;   ;      ;;  ;    ;      ;    ;;  ;   ;;  ; ;   ; 
+;   ;      ;   ;;   ;      ;    ;   ;;  ;     ;     
+;   ;      ;;;;;;   ;      ;    ;;;;;;  ;      ;;;  
+;   ;      ;        ;      ;    ;       ;         ; 
+;   ;      ;        ;      ;    ;       ;     ;   ; 
+;   ;;;;;;  ;;;;    ;;;    ;;;   ;;;;   ;      ;;;  
+;                                                   
+;                                                   
+;                                                   
